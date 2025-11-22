1e8b576cc2f608546f7223c720114d45741f205b
Yasin Eren Kovalık <yasinerenkovalik@Yasins-MacBook-Air.local>
2025-11-22 12:55:21 +0300

    Fix build errors: upgrade to Node 20 and use esbuild minifier

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
+    minify: 'esbuild',
     rollupOptions: {
       output: {
         manualChunks: {
